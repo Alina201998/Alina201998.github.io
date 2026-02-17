1094a9a9b30e7ac917ba39246cf464fad3f0358f
Alina201998 <alinakip20@gmail.com>
2026-02-16 18:15:34 -0600

    Update Footer.jsx

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -21,7 +21,7 @@ const Footer = () => {
             </div>
             <hr />
             <div className="footer-bottom">
-                <p className="footer-bottom-left">© 2025 Alina Kiptenko. All rights reserved.</p>
+                <p className="footer-bottom-left">© 2026 Alina Kiptenko. All rights reserved.</p>
                 <div className="footer-bottom-right">
                     <p>Term of Services</p>
                     <p>Privacy Policy</p>
